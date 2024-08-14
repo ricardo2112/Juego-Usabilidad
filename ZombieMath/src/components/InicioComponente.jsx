0f8cb3166fb8c0821e0061ae9ed484e66895898e
Ricardo Becerra <119013561+ricardo2112@users.noreply.github.com>
2024-08-13 23:19:05 -0500

    Inicio-Menu-Victoria

--- ZombieMath/src/components/InicioComponente.jsx
+++ ZombieMath/src/components/InicioComponente.jsx
@@ -3,20 +3,55 @@ import { Link } from 'react-router-dom';
 import FondoInicio from '../assets/FondoInicio.png'
 
 function InicioComponente({ irCategoria }) {
+    const highlightFirstLetters = (text) => {
+        return text.split(/(\s+)/).map((word, index) => {
+            if (word.trim().length > 0) {
+                const color = index === 0 ? 'red' : 'red'; // Cambia el color según el índice o el contenido
+                return (
+                    <React.Fragment key={index}>
+                    <span 
+                        style={{ color: color, fontWeight: 'bold'}}>
+                        {word[0]}
+                    </span>
+                    {word.slice(1)}
+                    </React.Fragment>
+                );
+            }
+            return word;
+        });
+    };
+
     return (
         <div
-        className='d-flex flex-column justify-content-center align-items-center vh-100 vw-100'
-        style={{
-          backgroundImage: `url(${FondoInicio})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat'
-        }}> 
-            <h2 className='text-white'>Zombie-Math</h2>
-            <p className='text-white fw-regular mb-8 text-center px-4 py-4'>En el juego, eres un científico intentando curar a los zombis en una pantalla de computadora. Con respuestas correctas, avanzas hacia la cura, pero el tiempo es crucial. Si fallas, tus habilidades matemáticas serán tu única esperanza. ¡Prepárate para salvar el mundo!</p>
-            <h3 className='text-white'>Tu nombre:</h3>
-            <input type="Tu nombre" />
-            <button onClick={irCategoria}>Jugar</button>
+            className='d-flex flex-column justify-content-center align-items-center vh-100 vw-100'
+            style={{
+                backgroundImage: `url(${FondoInicio})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat'
+            }}>
+            <h2 className='highlight-text'>
+                {highlightFirstLetters('Zombie Math')}
+            </h2>
+            <div className='text-center mx-5 px-4'>
+                <p className='text-white fs-4 fw-regular mx-5 px-5 py-4'
+                style={{
+                    textShadow: '-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000'
+                }}>
+                    En el juego, eres un científico intentando curar a los zombis en una pantalla de computadora. Con respuestas correctas, avanzas hacia la cura, pero el tiempo es crucial. Si fallas, tus habilidades matemáticas serán tu única esperanza. ¡Prepárate para salvar el mundo!
+                </p>
+            </div>
+            <div className='d-flex justify-content-center py-5'>
+                <span className='fw-regular text-white px-4 fs-3'>Tu nombre:</span>
+                <input 
+                    type="text" 
+                    className='form-control text-white' 
+                    style={{ maxWidth: '200px', fontSize: '1.2rem', backgroundColor: 'rgba(0, 0, 0, 0.3)'}}
+                />
+            </div>
+            <button className='btn btn-outline-danger btn-lg d-flex justify-content-center align-items-center fw-bold text-white fs-4 px-5 py-4' onClick={irCategoria} >
+                {highlightFirstLetters('Jugar')}
+            </button>
         </div>
     );
 }
--- ZombieMath/src/pages/Victoria.jsx
+++ ZombieMath/src/pages/Victoria.jsx
@@ -1,12 +1,44 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import FondoVictoria from '../assets/FondoVictoria.png';
+
 
 const Victoria = () => {
+  const highlightFirstLetters = (text) => {
+    return text.split(/(\s+)/).map((word, index) => {
+        if (word.trim().length > 0) {
+            const color = index === 0 ? 'red' : 'red';
+            return (
+                <React.Fragment key={index}>
+                    <span style={{ color: color, fontWeight: 'bold' }}>{word[0]}</span>{word.slice(1)}
+                </React.Fragment>
+            );
+        }
+        return word;
+    });
+  };
+  
   return (
-    <div>
-        <h1>Victoria</h1>
-        <Link to={'/'}>Inicio</Link>
+    <div style={{
+      height: '100vh',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      backgroundImage: `url(${FondoVictoria})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      color: 'white'
+    }}>
+    <h1 className='highlight-text' style={{ textAlign: 'center'}}>{highlightFirstLetters('Victoria')}</h1>
+      <p style={{ textAlign: 'center', fontSize: '32px' }}>¡Felicidades, científico! Has demostrado tu habilidad matemática y tu valentía en la lucha contra los zombies. Gracias a tu ingenio y determinación, has encontrado la cura y salvado a la humanidad de la amenaza zombi. 
+      ¡Eres un verdadero héroe de las matemáticas!</p>
+      <div style={{ display: 'flex', justifyContent: 'center', gap: '32px' }}>
+        <Link type='button' className='btn btn-outline-danger btn-lg d-flex justify-content-center align-items-center fw-bold text-white fs-4 px-5 py-4' to={'/'}>{highlightFirstLetters('Jugar')}</Link>
+        <Link type='button' className='btn btn-outline-danger btn-lg d-flex justify-content-center align-items-center fw-bold text-white fs-4 px-5 py-4' to={'/'}>{highlightFirstLetters('Salir')}</Link>
+      </div>
     </div>
+
   )
 }
 
